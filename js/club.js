cb79cd74be1703dbed9b7f13552f83a687581a89
subha_das <sd7843@pleasantonusd.net>
2020-08-22 15:17:04 -0700

    first commit

---
+++ js/club.js
@@ -0,0 +1,29 @@
+function club(sheetsOb) {
+    var categories = {
+
+    }
+
+    var clubs = {
+
+    }
+
+    sheetsOb[0].forEach(function(category, index) {
+        categories[`${index}`] = category;
+    });
+
+    for (var club of sheetsOb.slice(1)) {
+        var clubOb = {
+
+        }
+
+        club.forEach(function(value, index) {
+            clubOb[categories[`${index}`]] = value; 
+        });
+
+        clubs[clubOb["What is the name of your club?"]] = clubOb;
+    }
+
+    return clubs;
+}
+
+module.exports = club;
---
+++ server.js
@@ -0,0 +1,42 @@
+var express = require("express");
+var app = express();
+const fs = require("fs");
+const index = fs.readFileSync("index.html");
+const credentials = JSON.parse(fs.readFileSync("credentials.json"));
+const { google } = require('googleapis');
+const { sheets } = require("googleapis/build/src/apis/sheets");
+var auth = new google.auth.JWT(credentials.client_email, null, credentials.private_key, ["https://www.googleapis.com/auth/spreadsheets"]);
+var data = [];
+const club = require("./js/club.js");
+
+auth.authorize(setData);
+
+app.get("/", function (req, res) {
+    res.end(index);
+});
+
+app.post("/getData", function(req, res) {
+    res.setHeader("Content-Type", "application/json");
+    res.end(JSON.stringify(data));
+});
+
+app.listen(81, function () {
+    console.log("server started on port 80");
+});
+
+function setData(err, token) {
+    if (err) return;
+    const sheets = google.sheets({
+        version: 'v4',
+        auth: auth
+    });
+
+    sheets.spreadsheets.values.get({
+        spreadsheetId: "1mkEshytIqNDkkNUTpuhiZjYMJNNxSvAvzTZgYXizgh4",
+        range: "A1:N100"
+    }).then((response) => {
+        data = club(response.data.values);
+    });
+}
+
+setInterval(() => { if (auth.isTokenExpiring()) auth.authorize(setData); }, 4000);
