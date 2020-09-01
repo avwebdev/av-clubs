a95e64da80192c5024ff0b7a53c062e2cc1bb0ee
Keerthi Kalyaan <keerthi.kalyaan2004@gmail.com>
2020-08-31 20:21:01 -0700

    Co-authored-by: Yash Totale <totaleyash@gmail.com>

--- front-end/index.js
+++ front-end/index.js
@@ -2,6 +2,17 @@ const clubsDiv = document.getElementById("all-clubs");
 const searchInput = document.getElementById("search-input");
 const clubSuggestions = document.getElementById("club-suggestions");
 
+// Club fields
+const description = "What is the purpose of your club?";
+const contact = "What is the club contact email?";
+const meetingTime = "When are meetings held? (What day and how often?)";
+const meetingLocation = "Where are meetings held?";
+const president = "Who is the current Club President (or equivalent)?";
+const vicePresident = "Who is the current Club Vice President (or equivalent)?";
+const secretary = "Who is the current Club Secretary (or equivalent)?";
+const treasurer = "Who is the current Club Treasurer (or equivalent)?";
+const otherOfficers = "Are there any other Club Officer? (Please state their roles.)";
+
 async function getData() {
   const response = await fetch("/getData", { method: "POST" });
   const json = await response.json();
@@ -89,16 +100,46 @@ getData().then((allClubs) => {
   }
 
   function createClubDiv(name, club) {
-    console.log(`club-div-${name}`);
+    console.log(club);
     const clubDiv = createElement("div", { className: "club-div", id: `club-div-${name}` });
+    clubDiv.setAttribute("data-count", "0");
+    const clubDisplay = createElement("div", {className: "club-div-display"});
     const clubTitle = createElement("h1", {
       className: "club-title",
       innerText: name
     });
-    clubDiv.appendChild(clubTitle);
-    const i = createElement("i", { className: "mdi mdi-arrow-down-drop-circle-outline" })
-    clubDiv.appendChild(i);
+    clubDisplay.appendChild(clubTitle);
+    const i = createElement("i", { className: "mdi mdi-arrow-down-drop-circle-outline club-dropdown-icon", onclick: () => showClubInfo(clubDiv) });
+    clubDisplay.appendChild(i);
+    clubDiv.appendChild(clubDisplay);
+    //Dropdown
+    const clubDropdown = createElement("div", {className: "club-dropdown"});
+    const clubDescription = createElement("p", {innerText: `Description: ${club[description]}`, className: "club-dropdown-item club-description"});
+    const clubContact = createElement("p", {innerText: `Contact Email: ${club[contact]}`, className: "club-dropdown-item club-contact"});
+    const clubMeetingTime = createElement("p", {innerText: `Meeting Time: ${club[meetingTime]}`, className: "club-dropdown-item club-meeting-time"});
+    const clubMeetingLocation = createElement("p", {innerText: `Meeting Location: ${club[meetingLocation]}`, className: "club-dropdown-item club-meeting-location"});
+    const clubOfficers = createElement("p", {
+      innerText: `Officers: ${club[president]} (President), ${club[vicePresident]} (Vice President), ${club[secretary]} (Secretary), ${club[treasurer]} (Treasurer), ${club[otherOfficers]}`, 
+      className: "club-dropdown-item club-officers"});
+    clubDropdown.appendChild(clubDescription);
+    clubDropdown.appendChild(clubContact);
+    clubDropdown.appendChild(clubMeetingTime);
+    clubDropdown.appendChild(clubMeetingLocation);
+    clubDropdown.appendChild(clubOfficers);
+
+    clubDiv.appendChild(clubDropdown);
     return clubDiv;
+  }
+
+  function showClubInfo(element) {
+    const clubDropdown = element.getElementsByClassName("club-dropdown")[0];
+    let count = parseInt(element.getAttribute("data-count"));
+    if(count % 2 === 0) {
+      clubDropdown.style.display = "flex";
+    } else {
+      clubDropdown.style.display = "none";
+    }   
+    element.setAttribute("data-count", (count + 1).toString());    
   }
 
   function createElement(type, attributes) {
@@ -172,4 +213,33 @@ function applyTag(mode, tagName) {
 
 function clearTags() {
   document.querySelector("#sort-area").innerHTML = "";
+}
+
+function rotate(element){
+  display(element, count);
+  if(count%2==0){
+    element.style.transform = "rotate(-180deg)";
+    count++;
+  }
+  else if (count % 2 == 1) {
+      element.style.transform = "none";
+      count++;
+  }
+}
+
+function display(i, counter) {
+  var element = i.parentNode;
+  var feed = document.getElementsByClassName("announcement");
+  var answer = document.getElementsByClassName("answer");
+  for (var x = 0; x < feed.length; x++) {
+    if (element.isEqualNode(feed[x])) {
+      if (counter % 2 == 0) {
+        answer[x].style.display = "block";
+        answer[x].style.borderTop = "1px solid black";
+      }
+      else if (counter % 2 == 1) {
+        answer[x].style.display = "none";
+      }
+    }
+  }
 }
