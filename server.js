55ea95d35f8d9ec6f6fba7f7415ef432aef18b62
YashTotale <totaleyash@gmail.com>
2020-09-25 19:09:44 -0700

    changes

--- server.js
+++ server.js
@@ -2,55 +2,66 @@ var express = require("express");
 var app = express();
 const fs = require("fs");
 const credentials = JSON.parse(fs.readFileSync("credentials.json"));
-const { google } = require('googleapis');
+const { google } = require("googleapis");
 const { sheets } = require("googleapis/build/src/apis/sheets");
-var auth = new google.auth.JWT(credentials.client_email, null, credentials.private_key, ["https://www.googleapis.com/auth/spreadsheets"]);
+var auth = new google.auth.JWT(
+  credentials.client_email,
+  null,
+  credentials.private_key,
+  ["https://www.googleapis.com/auth/spreadsheets"]
+);
 var data = [];
 var announcements;
 const club = require("./js/club.js");
-const announcement = require("./js/announcements.js")
+const announcement = require("./js/announcements.js");
 
 auth.authorize(setData);
 
-app.use("/", express.static( __dirname + "/front-end"));
+app.use("/", express.static(__dirname + "/front-end"));
 
-app.post("/getData", function(req, res) {
-    res.setHeader("Content-Type", "application/json");
-    res.end(JSON.stringify(data));
+app.post("/getData", function (req, res) {
+  res.setHeader("Content-Type", "application/json");
+  res.end(JSON.stringify(data));
 });
 
-app.post("/announcements", function(req, res) {
-    res.setHeader("Content-Type", "application/json");
-    res.end(JSON.stringify(announcements));
+app.post("/announcements", function (req, res) {
+  res.setHeader("Content-Type", "application/json");
+  res.end(JSON.stringify(announcements));
 });
 
 app.listen(81, function () {
-    console.log("server started on port 80");
+  console.log("server started on port 80");
 });
 
 function setData(err, token) {
-    if (err) return;
-    const sheets = google.sheets({
-        version: 'v4',
-        auth: auth
-    });
+  if (err) return;
+  const sheets = google.sheets({
+    version: "v4",
+    auth: auth,
+  });
 
-    sheets.spreadsheets.values.get({
-        spreadsheetId: "1mkEshytIqNDkkNUTpuhiZjYMJNNxSvAvzTZgYXizgh4",
-        range: "A1:N100"
-    }).then((response) => {
-        data = club(response.data.values);
-        loadAnnouncements(sheets);
+  sheets.spreadsheets.values
+    .get({
+      spreadsheetId: "1mkEshytIqNDkkNUTpuhiZjYMJNNxSvAvzTZgYXizgh4",
+      range: "A1:N100",
+    })
+    .then((response) => {
+      data = club(response.data.values);
+      loadAnnouncements(sheets);
     });
 }
 
 async function loadAnnouncements(sheets) {
-    sheets.spreadsheets.values.get({
-        spreadsheetId: "1JEatzOlJ5vKTxQE4EUSJjoa_ViSJ_TGfwoG1Zx8x9kM",
-        range: "A1:N100"
-    }).then((response) => {
-         announcements = announcement(response.data.values);
+  sheets.spreadsheets.values
+    .get({
+      spreadsheetId: "1JEatzOlJ5vKTxQE4EUSJjoa_ViSJ_TGfwoG1Zx8x9kM",
+      range: "A1:N100",
+    })
+    .then((response) => {
+      announcements = announcement(response.data.values);
     });
 }
 
-setInterval(() => { if (auth.isTokenExpiring()) auth.authorize(setData); }, 4000);
+setInterval(() => {
+  if (auth.isTokenExpiring()) auth.authorize(setData);
+}, 4000);
