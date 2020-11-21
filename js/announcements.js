4a5d2d14d0a63416662f7723f47043caa7be50c3
Subha Das <subatuba21@Subhas-MacBook-Air.local>
2020-11-21 14:47:23 -0800

    mailing list is working

--- js/announcements.js
+++ js/announcements.js
@@ -1,5 +1,16 @@
 const fs = require("fs");
 const nodemailer = require("nodemailer");
+var mailClient = nodemailer.createTransport({
+
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true, // true for 465, false for other ports
+    auth: {
+        user: "amadorvalleyweb@gmail.com", // generated ethereal user
+        pass: "****with@nchor", // generated ethereal password
+    }
+
+});
 
 function announcement(sheetsOb) {
     var categories = {
@@ -73,7 +84,7 @@ var mailingList = {
                         break;
                     }
                 }
- 
+
                 if (previousAnnouncement == null && announcement["Approved"] && announcement["Approved"].trim() != "") {
                     newAnnouncements.push(announcement);
                 }
@@ -85,9 +96,36 @@ var mailingList = {
     email: async function (announcements) {
         var emails = await this.getAllEmails();
         console.log(`sending ${announcements.length} announcements to ${emails}`);
+        for (var announcement of announcements) {
+            let html = ``;
+            html += `</p>${announcement["Paragraph 1"]}<br><br>
+                        ${announcement["Paragraph 2"] ? announcement["Paragraph 2"] + "<br><br>" : ""}
+                        ${announcement["Paragraph 3"] ? announcement["Paragraph 3"] + "<br><br>" : ""}
+                        ${announcement["Paragraph 4"] ? announcement["Paragraph 4"] + "<br><br>" : ""}
+                        ${announcement["Paragraph 5"] ? announcement["Paragraph 5"] + "<br><br>" : ""}</p>
+    
+    
+                        ${announcement["Link 1"] ? `${announcement["Link 1"]}<br>` : ""}
+                        ${announcement["Link 2"] ? `${announcement["Link 2"]}<br>` : ""}
+                        ${announcement["Link 3"] ? `${announcement["Link 3"]}<br>` : ""}
+                        ${announcement["Link 4"] ? `${announcement["Link 4"]}<br>` : ""}
+                        `;
+            console.log(html);
+            for (var mail of emails) {
+
+
+                await mailClient.sendMail({
+                    from: "AV Clubs <amadorvalleyweb.org>",
+                    to: emails,
+                    subject: `${announcement.Title}`,
+                    html: html
+                });
+
+            }
+        }
     },
 
-    registerNewEmail: async function(email) {
+    registerNewEmail: async function (email) {
         var formattedEmail = email.toLowerCase().trim();
         var emailAccessor = JSON.parse(fs.readFileSync("./js/mailingList.json"));
         if (!emailAccessor.emails.includes(formattedEmail)) {
@@ -97,18 +135,18 @@ var mailingList = {
 
     },
 
-    unsuscribeEmail: async function(email) {
+    unsuscribeEmail: async function (email) {
         var formattedEmail = email.toLowerCase().trim();
         var emailAccessor = JSON.parse(fs.readFileSync("./js/mailingList.json"));
         var indexFound = emailAccessor.emails.indexOf(formattedEmail);
-        if (indexFound!=-1) {
+        if (indexFound != -1) {
             emailAccessor.emails.splice(indexFound, 1);
             fs.writeFileSync("./js/mailingList.json", JSON.stringify(emailAccessor));
         }
     },
 
-    getAllEmails: async function() {
-        var {emails}= JSON.parse(await fs.promises.readFile("./js/mailingList.json"));
+    getAllEmails: async function () {
+        var { emails } = JSON.parse(await fs.promises.readFile("./js/mailingList.json"));
         return emails;
     }
 
